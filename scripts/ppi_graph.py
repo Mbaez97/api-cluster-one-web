391f128840b18085813cf853ba077de0a456df7e
MBaez97 <marcelobaezparaguay@gmail.com>
2023-06-04 20:45:14 -0400

    scripts for create ppi in db

--- scripts/ppi_graph.py
+++ scripts/ppi_graph.py
@@ -21,10 +21,6 @@ def get_random_color():
     r3 = random.randint(0, 255)
     return '#%02X%02X%02X' % (r1, r2, r3)
 
-
-def get_complex_protein_color():
-    return '#%02X%02X%02X' % (255, 0, 0)
-
 def generate_random_styles():
     _style = {
         "width": "mapData(score, 0, 0.006769776522008331, 20, 60)",
@@ -41,53 +37,6 @@ def generate_random_styles():
         "z-index": "10"
     }
     return _style
-
-def crate_protein_by_ppi(file_path: str):
-    '''
-    Crea los nodos de proteinas a partir de un archivo de PPI.
-    '''
-    # Lee el archivo TXT
-    dataset = lee_txt(file_path)
-    _creados = []
-    # Crea la sesion de la base de datos
-    db = SessionLocal()
-    for data in dataset:
-        _data = data.split("\t")
-        _url = 'www.ebi.ac.uk/proteins/api/proteins/'
-        # Crea el nodo de la proteina
-        if _data[0] in _creados:
-            continue
-        _creados.append(_data[0])
-        _style = generate_random_styles()
-        _style_objet = Style(
-            ccs_styles=_style,
-            type=0,
-        )
-        db.add(_style_objet)
-
-        protein_1 = Protein(
-            name=_data[0],
-            description="Automatic node created from PPI file",
-            score=0.0,
-            url_info=_url + _data[0],
-            style=_style_objet
-        )
-        db.add(protein_1)
-        if _data[1] in _creados:
-            continue
-        _creados.append(_data[1])
-        protein_2 = Protein(
-            name=_data[1].replace("\n", ""),
-            description="Automatic node created from PPI file",
-            score=0.0,
-            url_info=_url + _data[1],
-            style=_style_objet
-        )
-        db.add(protein_2)
-        db.commit()
-        print("Protein created: ", protein_1.name)
-        print("Protein created: ", protein_2.name)
-    db.close()
 
 def create_ppi(file_path: str):
     ppi_dataset = lee_csv(file_path)
