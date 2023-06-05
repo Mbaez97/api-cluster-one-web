df85961c12eeaaaca402384b713927c6497052f2
MBaez97 <marcelobaezparaguay@gmail.com>
2023-06-04 22:01:01 -0400

    add quick run endpoint to cluster one

---
+++ app/api/utils.py
@@ -0,0 +1,38 @@
+import random
+import os
+from time import sleep
+
+def get_random_color():
+    r1 = random.randint(0, 255)
+    r2 = random.randint(0, 255)
+    r3 = random.randint(0, 255)
+    return '#%02X%02X%02X' % (r1, r2, r3)
+
+
+def get_complex_protein_color():
+    return '#%02X%02X%02X' % (255, 0, 0)
+
+def generate_random_styles():
+    _style = {
+        "width": "mapData(score, 0, 0.006769776522008331, 20, 60)",
+        "height": "mapData(score, 0, 0.006769776522008331, 20, 60)",
+        "content": "data(name)",
+        "font-size": "12px",
+        "text-valign": "center",
+        "text-halign": "center",
+        "background-color": get_random_color(),
+        "text-outline-color": "#555",
+        "text-outline-width": "2px",
+        "color": "#fff",
+        "overlay-padding": "6px",
+        "z-index": "10"
+    }
+    return _style
+
+def execute_cluster_one(command: str, *args):
+    os.system(command)
+    sleep(2)
+    with open("complex_cluster_response.txt", "r") as f:
+        response = f.read()
+    os.system("rm complex_cluster_response.txt")
+    return response.split("\n")
