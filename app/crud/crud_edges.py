e9b3cd48e3badb608c7f2a911b2a867e673e34c9
MBaez97 <marcelobaezparaguay@gmail.com>
2023-09-06 23:38:15 -0400

    get edges by protein pair

--- app/crud/crud_edges.py
+++ app/crud/crud_edges.py
@@ -6,13 +6,17 @@ from app.models import Edge, EdgeClusterInteraction
 
 
 class CRUDEdge(CRUDBase[Edge, schemas.EdgeCreate, schemas.EdgeUpdate]):
-    def get_all_cluster(self, db) -> Edge:
-        """Get all cluster"""
-        return db.query(Edge).all()
+    def get_by_proteins(self, db, *, protein_a_id: int, protein_b_id: int) -> Edge:
+        """Get edge by protein id"""
+        return (
+            db.query(Edge)
+            .filter(Edge.protein_a_id == protein_a_id)
+            .filter(Edge.protein_b_id == protein_b_id)
+            .first()
+        )
 
     def create_edge_for_cluster(self, db, *, obj: dict) -> Edge:
         """Create cluster"""
-        print("LOGS: Creating edge for cluster")
         db_obj = Edge(
             protein_a_id=obj["protein_a_id"],
             protein_b_id=obj["protein_b_id"],
@@ -20,15 +24,29 @@ class CRUDEdge(CRUDBase[Edge, schemas.EdgeCreate, schemas.EdgeUpdate]):
             has_direction=obj["has_direction"],
             direction=obj["direction"],
         )
+        db.add(db_obj)
+        db.commit()
+        db.refresh(db_obj)
         interaction = EdgeClusterInteraction(
             edge_id=db_obj.id,
             cluster_graph_id=obj["refers_to"].id,
         )
-        db.add(db_obj)
         db.add(interaction)
         db.commit()
-        db.refresh(db_obj)
+        db.refresh(interaction)
+        return db_obj
+
+    def add_edge_to_cluster(self, db, *, obj: dict) -> Edge:
+        """Create cluster"""
+        db_obj = db.query(Edge).filter(Edge.id == obj["id"]).first()
+        interaction = EdgeClusterInteraction(
+            edge_id=db_obj.id,
+            cluster_graph_id=obj["refers_to"].id,
+        )
+        db.add(interaction)
+        db.commit()
+        db.refresh(interaction)
         return db_obj
 
 
-Edge = CRUDEdge(Edge)
+edge = CRUDEdge(Edge)
