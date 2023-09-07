28665b67cabb591cd070b4e6145e8b300ce811d6
MBaez97 <marcelobaezparaguay@gmail.com>
2023-09-07 01:14:18 -0400

    clean code and add bluk creation

--- app/crud/crud_edges.py
+++ app/crud/crud_edges.py
@@ -3,6 +3,7 @@
 from app import schemas
 from app.crud.crud_base import CRUDBase
 from app.models import Edge, EdgeClusterInteraction
+from typing import List
 
 
 class CRUDEdge(CRUDBase[Edge, schemas.EdgeCreate, schemas.EdgeUpdate]):
@@ -36,6 +37,32 @@ class CRUDEdge(CRUDBase[Edge, schemas.EdgeCreate, schemas.EdgeUpdate]):
         db.refresh(interaction)
         return db_obj
 
+    def bulk_create_edge_for_cluster(
+        self, db, *, obj: dict, edge_list: List[dict], cluster_id: int
+    ) -> bool:
+        # Creamos de una
+        try:
+            data_edges = db.bulk_insert_mappings(Edge, edge_list)
+            db.commit()
+            db.refresh(data_edges)
+            _interactions = []
+            for edge in edge_list:
+                _interactions.append(
+                    {
+                        "edge_id": edge.id,
+                        "cluster_graph_id": cluster_id,
+                    }
+                )
+            data_interactions = db.bulk_insert_mappings(
+                EdgeClusterInteraction, _interactions
+            )
+            db.commit()
+            db.refresh(data_interactions)
+            return True
+        except Exception as e:
+            print(e)
+            return False
+
     def add_edge_to_cluster(self, db, *, obj: dict) -> Edge:
         """Create cluster"""
         db_obj = db.query(Edge).filter(Edge.id == obj["id"]).first()
@@ -47,6 +74,29 @@ class CRUDEdge(CRUDBase[Edge, schemas.EdgeCreate, schemas.EdgeUpdate]):
         db.commit()
         db.refresh(interaction)
         return db_obj
+
+    def bulk_add_edge_to_cluster(
+        self, db, *, obj: dict, edge_list: List[Edge], cluster_id: int
+    ) -> bool:
+        # Creamos de una
+        try:
+            _interactions = []
+            for edge in edge_list:
+                _interactions.append(
+                    {
+                        "edge_id": edge.id,
+                        "cluster_graph_id": cluster_id,
+                    }
+                )
+            data_interactions = db.bulk_insert_mappings(
+                EdgeClusterInteraction, _interactions
+            )
+            db.commit()
+            db.refresh(data_interactions)
+            return True
+        except Exception as e:
+            print(e)
+            return False
 
 
 edge = CRUDEdge(Edge)
