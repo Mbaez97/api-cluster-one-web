ded76a3bc258205f38fa69e95ebd09ce77250889
MBaez97 <marcelobaezparaguay@gmail.com>
2023-09-25 02:22:57 -0400

    Update CRUD Functions

--- app/crud/crud_edges.py
+++ app/crud/crud_edges.py
@@ -2,7 +2,7 @@
 
 from app import schemas
 from app.crud.crud_base import CRUDBase
-from app.models import Edge, EdgeClusterInteraction
+from app.models import Edge, EdgeClusterInteraction, EdgePPIInteraction
 from typing import List
 
 
@@ -35,6 +35,27 @@ class CRUDEdge(CRUDBase[Edge, schemas.EdgeCreate, schemas.EdgeUpdate]):
         db.commit()
         db.refresh(interaction)
         return db_obj
+    
+    def create_edge_for_ppi(self, db, *, obj: dict) -> Edge:
+        """Create cluster"""
+        db_obj = Edge(
+            protein_a_id=obj["protein_a_id"],
+            protein_b_id=obj["protein_b_id"],
+            direction=obj["direction"],
+        )
+        db.add(db_obj)
+        db.commit()
+        db.refresh(db_obj)
+        interaction = EdgePPIInteraction(
+            edge_id=db_obj.id,
+            weight=obj["weight"],
+            ppi_interaction_id=obj["refers_to"].id,
+        )
+        db.add(interaction)
+        db.commit()
+        db.refresh(interaction)
+        print(f"LOGS: {db_obj.id} - {obj['refers_to'].id} - {obj['weight']} - {db_obj.protein_a.name} - {db_obj.protein_b.name}")
+        return db_obj
 
     def bulk_create_edge_for_cluster(
         self, db, *, obj: dict, edge_list: List[dict], cluster_id: int
@@ -60,6 +81,30 @@ class CRUDEdge(CRUDBase[Edge, schemas.EdgeCreate, schemas.EdgeUpdate]):
             print(e)
             return False
 
+    def bulk_create_edge_for_ppi(
+        self, db, *, obj: dict, edge_list: List[dict], ppi_id: int
+    ) -> bool:
+        # Creamos de una
+        try:
+            data_edges = db.bulk_insert_mappings(Edge, edge_list)
+            db.commit()
+            db.refresh(data_edges)
+            _interactions = []
+            for edge in edge_list:
+                _interactions.append(
+                    {
+                        "edge_id": edge.id,
+                        "ppi_interaction_id": ppi_id,
+                        "weight": 1,
+                    }
+                )
+            db.bulk_insert_mappings(EdgePPIInteraction, _interactions)
+            db.commit()
+            return True
+        except Exception as e:
+            print(e)
+            return False
+
     def add_edge_to_cluster(self, db, *, obj: dict) -> Edge:
         """Create cluster"""
         db_obj = db.query(Edge).filter(Edge.id == obj["id"]).first()
@@ -71,6 +116,20 @@ class CRUDEdge(CRUDBase[Edge, schemas.EdgeCreate, schemas.EdgeUpdate]):
         db.add(interaction)
         db.commit()
         db.refresh(interaction)
+        return db_obj
+
+    def add_edge_to_ppi(self, db, *, obj: dict) -> Edge:
+        """Create cluster"""
+        db_obj = db.query(Edge).filter(Edge.id == obj["id"]).first()
+        interaction = EdgePPIInteraction(
+            edge_id=db_obj.id,
+            ppi_interaction_id=obj["refers_to"].id,
+            weight=obj["weight"],
+        )
+        db.add(interaction)
+        db.commit()
+        db.refresh(interaction)
+        print(f"LOGS: {db_obj.id} - {obj['refers_to'].id} - {obj['weight']} - {db_obj.protein_a.name} - {db_obj.protein_b.name}")
         return db_obj
 
     def bulk_add_edge_to_cluster(
@@ -93,6 +152,48 @@ class CRUDEdge(CRUDBase[Edge, schemas.EdgeCreate, schemas.EdgeUpdate]):
         except Exception as e:
             print(e)
             return False
+
+    def bulk_add_edge_to_ppi(
+        self, db, *, obj: dict, edge_list: List[Edge], ppi_id: int
+    ) -> bool:
+        # Creamos de una
+        try:
+            _interactions = []
+            for edge in edge_list:
+                _interactions.append(
+                    {
+                        "edge_id": edge.id,
+                        "ppi_graph_id": ppi_id,
+                        "weight": 1,
+                    }
+                )
+            db.bulk_insert_mappings(EdgePPIInteraction, _interactions)
+            db.commit()
+            return True
+        except Exception as e:
+            print(e)
+            return False
+
+    def get_edge_from_ppi(self, db, *, obj: dict, ppi_id: int, protein_a_id: int = 0, protein_b_id: int = 0):
+        """
+        Get Edge from PPI
+        """
+        if protein_a_id != 0 and protein_b_id != 0:
+            return (
+                db.query(Edge)
+                .join(EdgePPIInteraction)
+                .filter(Edge.protein_a_id == protein_a_id)
+                .filter(Edge.protein_b_id == protein_b_id)
+                .filter(EdgePPIInteraction.ppi_graph_id == ppi_id)
+                .first()
+            )
+        else:
+            return (
+                db.query(Edge)
+                .join(EdgePPIInteraction)
+                .filter(EdgePPIInteraction.ppi_graph_id == ppi_id)
+                .all()
+            )
 
 
 edge = CRUDEdge(Edge)
--- app/crud/crud_protein.py
+++ app/crud/crud_protein.py
@@ -2,7 +2,7 @@
 
 from app import schemas
 from app.crud.crud_base import CRUDBase
-from app.models import Protein, ClusterGraph
+from app.models import Protein, ClusterGraph, OverlappingProtein
 from typing import List
 
 
@@ -36,7 +36,7 @@ class CRUDProtein(
         print("Protein created: ", protein_1.name)
         return protein_1
 
-    def get_by_cluster(self, db, *, cluster_id: int) -> List[Protein]:
+    def get_all_by_cluster(self, db, *, cluster_id: int) -> List[Protein]:
         """Get protein by cluster id"""
         _cluster = db.query(ClusterGraph).filter(ClusterGraph.id == cluster_id).first()
         _edges = _cluster.edges
@@ -51,5 +51,50 @@ class CRUDProtein(
                 _proteins_id.append(i)
         return db.query(Protein).filter(Protein.id.in_(_proteins_id)).all()
 
+
+class CRUDOverlappingProtein(
+    CRUDBase[OverlappingProtein, schemas.OverlappingProteinCreate, schemas.OverlappingProteinUpdate]
+):  # noqa
+    def get_all(self, db) -> OverlappingProtein:
+        """Get all protein"""
+        return db.query(OverlappingProtein).all()
+
+    def get_by_id(self, db, *, id: int) -> OverlappingProtein:
+        """Get protein by id"""
+        return db.query(OverlappingProtein).filter(OverlappingProtein.id == id).first()
+
+    def get_by_name(self, db, *, name: str) -> OverlappingProtein:
+        """Get agencia by name"""
+        return db.query(OverlappingProtein).filter(OverlappingProtein.name == name).first()
+
+    def quick_creation(self, db, *, name: str) -> OverlappingProtein:
+        _data = name
+        _url = "www.ebi.ac.uk/proteins/api/proteins/"
+
+        protein_1 = OverlappingProtein(
+            name=_data,
+            description="Automatic node created from PPI file",
+            score=0.0,
+            url_info=_url + _data,
+        )
+        db.add(protein_1)
+        db.commit()
+        print("Protein created: ", protein_1.name)
+        return protein_1
+
+    def get_all_by_cluster(self, db, *, cluster_id: int) -> List[OverlappingProtein]:
+        """Get protein by cluster id"""
+        _cluster = db.query(ClusterGraph).filter(ClusterGraph.id == cluster_id).first()
+        _edges = _cluster.edges
+        _proteins_a_id = [i.protein_a_id for i in _edges]
+        _proteins_b_id = [i.protein_b_id for i in _edges]
+        _proteins_id = []
+        for i in _proteins_a_id:
+            if i not in _proteins_id:
+                _proteins_id.append(i)
+        for i in _proteins_b_id:
+            if i not in _proteins_id:
+                _proteins_id.append(i)
+        return db.query(OverlappingProtein).filter(OverlappingProtein.id.in_(_proteins_id)).all()
 
 protein = CRUDProtein(Protein)
