377087a55bae3a022352f50c9e841e1ee70bc1f4
MBaez97 <marcelobaezparaguay@gmail.com>
2023-09-25 02:23:53 -0400

    Add Overlapping Protein in models init

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -4,6 +4,4 @@ from .edge_ppi_interaction import EdgePPIInteraction  # noqa
 from .edge import Edge  # noqa
 from .graph import AbstractGraph, ClusterGraph, PPIGraph  # noqa
 from .layout import Layout  # noqa
-from .protein import Protein  # noqa
-
-# from .go_terms import GoTerm
+from .protein import Protein, OverlappingProtein  # noqa
