0553d3f0cb0002ad3c076e695eb210d3ec3cb367
MBaez97 <marcelobaezparaguay@gmail.com>
2023-11-16 01:40:12 -0300

    Add ORA Score columns to ClusterOneLogParams model

--- app/models/cluster_one_logs.py
+++ app/models/cluster_one_logs.py
@@ -1,8 +1,8 @@
 """Edge models"""
 
-from sqlalchemy import Column, Float, Boolean, Integer, ForeignKey, UniqueConstraint
-from sqlalchemy.ext.declarative import declared_attr
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Float, Boolean, Integer, ForeignKey, UniqueConstraint  # type: ignore # noqa
+from sqlalchemy.ext.declarative import declared_attr  # type: ignore
+from sqlalchemy.orm import relationship  # type: ignore
 
 from app.db.base_class import BaseWithDatetime
 
@@ -16,7 +16,11 @@ class ClusterOneLogParams(BaseWithDatetime):
     def __table_args__(cls):
         return (
             UniqueConstraint(
-                "min_size", "min_density", "max_overlap", "penalty", "ppi_graph_id"
+                "min_size",
+                "min_density",
+                "max_overlap",
+                "penalty",
+                "ppi_graph_id",  # noqa
             ),
         )
 
@@ -25,8 +29,13 @@ class ClusterOneLogParams(BaseWithDatetime):
     min_density = Column(Float, nullable=True)
     max_overlap = Column(Float, nullable=True)
     penalty = Column(Float, nullable=True)
+    ora_bp_score = Column(Float, nullable=True)
+    ora_cc_score = Column(Float, nullable=True)
+    ora_mf_score = Column(Float, nullable=True)
     ppi_graph_id = Column(Integer, ForeignKey("ppi_graph.id"), nullable=False)
-    ppi_graph = relationship("PPIGraph", back_populates="cluster_one_log_params")
+    ppi_graph = relationship(
+        "PPIGraph", back_populates="cluster_one_log_params"
+    )  # noqa
     cluster_graph = relationship(
         "ClusterGraph", back_populates="cluster_one_log_params"
     )
